5290c928f5c8ff4c9e669094e646373d9525b1a4
PhamVanTuan205 <vantuan.412005@gmail.com>
2024-11-13 12:49:06 +0700

    ILoginService

---
+++ ILoginService.cs
@@ -0,0 +1,10 @@
+﻿using KOI_POND_WEB.Models;
+
+namespace KOI_POND_WEB.Services.Interfaces
+{
+    public interface ILoginService
+    {
+        User Login(string username, string password);
+        bool Register(string username, string password, string name, string confirmPassword);
+    }
+}
