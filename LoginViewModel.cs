8f821fb5a97d6f667e7bdb2513cfc65dfaa95559
PhamVanTuan205 <vantuan.412005@gmail.com>
2024-11-13 12:46:39 +0700

    LoginViewModel

---
+++ LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace YourNamespace.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
