8030bbb5050d2114141089b9c62b6cdaed8e8747
Tiencodedao <163629736+Tiencodedao@users.noreply.github.com>
2024-11-17 22:40:32 +0700

    Delete KoiPond directory

--- KoiPond/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace KoiPond.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
